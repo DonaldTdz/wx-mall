2a9e7b7a1a235e369e63ee349c1103b92c689e6e
DonaldTdz <tangdezou@qq.com>
2019-01-09 12:51:57 +0800

    商品分类

--- src/api/dzapi.js
+++ src/api/dzapi.js
@@ -14,11 +14,14 @@ import {
   const getGoodsCategories = (params) => wxRequest(params, apiMall + '/api/services/app/Category/GetGoodsCategoriesAsync');
   //商品搜索
   const getSearchGoods = (params) => wxRequest(params, apiMall + '/api/services/app/Good/GetSearchGoodsAsync');
+  //商品分类
+  const getGroupGoods = (params) => wxRequest(params, apiMall + '/api/services/app/Good/GetGroupGoodsAsync');
   
   export default {
     wxJsCode2Session,
     getHeatGoods,
     getGoodsCategories,
-    getSearchGoods
+    getSearchGoods,
+    getGroupGoods
   }
   
